39c9f16b5100216dc89b2b2d6048f7c678b1f6d9
gusdev0258 <16326176760@edu.udesc.br>
2024-07-08 19:05:51 -0300

    feat: create card styling based on brewery type

--- linx-brewery/src/app/features/components/list/list.component.ts
+++ linx-brewery/src/app/features/components/list/list.component.ts
@@ -2,11 +2,12 @@ import { AfterViewInit, Component } from '@angular/core';
 import { BreweryService } from '../../../../services/brewery.service';
 import { BreweryInterface } from '../../../../interfaces/brewery-response.interface';
 import { CardComponent } from '../card/card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, CommonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
@@ -18,5 +19,23 @@ constructor(private readonly _breweryService: BreweryService) {}
     this._breweryService.getAllBreweries().subscribe((breweryResponse) => {
       this.breweryList = breweryResponse;
     });
+  }
+  getClassByBreweryType(breweryType: string): string {
+    switch (breweryType) {
+      case 'large':
+        return 'brewery__type__large';
+      case 'closed':
+        return 'brewery__type__closed';
+      case 'micro':
+        return 'brewery__type__micro';
+      case 'contract':
+        return 'brewery__type__contract';
+      case 'brewpub':
+        return 'brewery__type__brewpub';
+      case 'regional':
+        return 'brewery__type__regional';
+      default:
+        return '';
+    }
   }
 }
