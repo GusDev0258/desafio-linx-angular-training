f0d0d6c664e288414cbc79cd44f069078d322204
gusdev0258 <16326176760@edu.udesc.br>
2024-07-08 17:43:40 -0300

    style: styling card component

--- linx-brewery/src/app/features/components/list/list.component.ts
+++ linx-brewery/src/app/features/components/list/list.component.ts
@@ -1,11 +1,12 @@
 import { AfterViewInit, Component } from '@angular/core';
 import { BreweryService } from '../../../../services/brewery.service';
 import { BreweryInterface } from '../../../../interfaces/brewery-response.interface';
+import { CardComponent } from '../card/card.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
